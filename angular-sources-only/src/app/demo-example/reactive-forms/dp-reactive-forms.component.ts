663153062876e9a534730515d439049232e64c14
shenweiliu <gliuxs2@gmail.com>
2025-02-04 00:06:45 -0500

    Add files via upload

---
+++ angular-sources-only/src/app/demo-example/reactive-forms/dp-reactive-forms.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit, Renderer2, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { IAngularMyDpOptions, IMyDateModel, NgExMyDatePickerDirective } from 'ngex-mydatepicker';
+
+@Component({
+    selector: 'dp-reactive-forms',
+    templateUrl: './dp-reactive-forms.component.html',
+    styleUrls: ['./dp-reactive-forms.component.css'],
+    imports: [CommonModule, ReactiveFormsModule, NgExMyDatePickerDirective]
+})
+export class DpReactiveFormsComponent implements OnInit {
+  public myDatePickerOptions: IAngularMyDpOptions = {
+    dateRange: false,
+    dateFormat: 'mm/dd/yyyy'
+  };
+
+  public disabled: boolean = false;
+
+  public myForm!: FormGroup;
+
+    @ViewChild('dp') myDp!: NgExMyDatePickerDirective;
+
+  constructor(private formBuilder: FormBuilder, private renderer: Renderer2, private cdr: ChangeDetectorRef) {
+  }
+
+  ngOnInit(): void {
+    console.log('onInit(): SampleDatePickerReacticeForms');
+    let d: Date = new Date();
+    d.setDate(d.getDate() + 2);
+    let model: IMyDateModel = {isRange: false, singleDate: {jsDate: d}, dateRange: null};
+
+    this.myForm = this.formBuilder.group({
+      myDate: [
+        model,
+        Validators.required
+      ]   // this example is initialized to specific date
+    });
+  }
+
+  onSubmitReactiveForms(): void {
+    console.log('Value: ',
+      this.myForm.controls['myDate'].value,
+      ' - Valid: ', this.myForm.controls['myDate'].valid,
+      ' - Dirty: ', this.myForm.controls['myDate'].dirty,
+      ' - Touched: ', this.myForm.controls['myDate'].touched
+    );
+  }
+  
+  toggleCalendar(): void {
+    this.cdr.detectChanges();
+    this.myDp.toggleCalendar();
+  }
+
+  setTodayDate(): void {
+    // Set today using the setValue function
+    let date: Date = new Date();
+    let model: IMyDateModel = {isRange: false, singleDate: {jsDate: date}, dateRange: null};
+
+    this.myForm.setValue({myDate: model});
+  }
+
+  resetTomorrowDate(): void {
+    // Reset date picker to specific date (tomorrow)
+    let date: Date = new Date();
+    date.setDate(date.getDate() + 1);
+    let model: IMyDateModel = {isRange: false, singleDate: {jsDate: date}, dateRange: null};
+    this.myForm.reset({myDate: model});
+  }
+
+  clearDate(): void {
+    // Clear the date using the setValue function
+    this.myForm.setValue({myDate: null});
+  }
+
+  disable(): void {
+    this.disabled = !this.disabled;
+    if (this.disabled) {
+      this.myForm.get('myDate')!.disable();
+    } else {
+      this.myForm.get('myDate')!.enable();
+    }
+  }
+}
