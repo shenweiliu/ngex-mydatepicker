4ce97b53436e0964ff7aac52fcf734cfb5b8ddcf
shenweiliu <gliuxs2@gmail.com>
2025-02-03 21:56:00 -0500

    Add files via upload

---
+++ NgExMyDatepicker_AspNetCore/SM.NgExMyDatepicker.Sample/CommonClasses/AppConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SM.NgExMyDatepicker.Sample
+{
+    public class AppConfig
+    {
+        public string TestConfig1 { get; set; }
+        public bool UseInMemoryDatabase { get; set; }
+    }
+}
